a514b24a81a4235cd27f64879d2166be0629de0c
Michael Onyeweke <michaelonyeweke@yahoo.com>
2022-09-22 20:34:00 +0100

    Correction of Infinite Addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    addition = 0
+   
+   addition = 0
     for arg in sys.argv:
         if arg != sys.argv[0]:
            addition += int(arg)
