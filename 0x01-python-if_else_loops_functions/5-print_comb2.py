12a46239d5ffd8b4dc6d97695002e64d745ddd7b
Michael Onyeweke <michaelonyeweke@yahoo.com>
2022-09-25 16:17:10 +0100

    correction

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for num in range(100):
     if (num != 99):
-        print(f"{num:02}, ", end='')
+        print("{}{}, ".format(int(num / 10), num % 10), end='')
     else:
-        print(99)
+        print("{}{}".format(int(num / 10), num % 10))
