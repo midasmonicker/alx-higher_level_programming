4c14cf31a2cbb8397fb39e69c4f846f15ce1d760
Michael Onyeweke <michaelonyeweke@yahoo.com>
2022-12-29 13:22:47 +0100

    Object Scopes

---
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+/**
+ * Defines a rectangle with width & height
+ */
+class Rectangle {
+  constructor (w, h) {
+    this.width = w;
+    this.height = h;
+  }
+}
+module.exports = Rectangle;
